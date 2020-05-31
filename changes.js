a887118855c5bbdeb140ba3759f4a30291882653
Roberto <robertomkumbukwa14@gmail.com>
2020-05-31 16:40:02 -0600

    Add files via upload

---
+++ changes.js
@@ -0,0 +1,7 @@
+//making an list
+//var dice=["images/dice1.png","images/dice2.png","images/dice3.png","images/dice4.png","images/dice5.png","images/dice6.png"];
+var ranN=Math.ceil(Math.random()*6);
+document.querySelector(".img1").src = ("images/dice"+ ranN + ".png");
+//document.querySelector("img").setAttribute("src","images/dice"+ ranN + ".png");
+//document.querySelector("img").setAttribute("src","images/dice2.png");
+//document.querySelector(".img1").setAttribute("src", "images/dice1.png");
